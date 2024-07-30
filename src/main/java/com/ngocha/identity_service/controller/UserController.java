ea4e5e49243ff408b269a84a3891f4a50c6d8fae
Ngocha2607 <ngocha2k.ln@gmail.com>
2024-07-30 23:26:51 +0700

    update authorized

--- src/main/java/com/ngocha/identity_service/controller/UserController.java
+++ src/main/java/com/ngocha/identity_service/controller/UserController.java
@@ -1,61 +1,65 @@
 package com.ngocha.identity_service.controller;
-
-import java.util.List;
-
-import jakarta.validation.Valid;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
 import com.ngocha.identity_service.dto.request.ApiResponse;
 import com.ngocha.identity_service.dto.request.UserCreationRequest;
 import com.ngocha.identity_service.dto.request.UserUpdateRequest;
 import com.ngocha.identity_service.dto.response.UserResponse;
-import com.ngocha.identity_service.entity.User;
 import com.ngocha.identity_service.service.Userservice;
-
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
-@RequestMapping("/api/users")
 public class UserController {
-    Userservice userservice;
+    Userservice userService;
 
     @PostMapping
-    ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request) {
-        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
-
-        apiResponse.setResult(userservice.createRequest(request));
-        return apiResponse;
+    ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request){
+        return ApiResponse.<UserResponse>builder()
+                .result(userService.createRequest(request))
+                .build();
     }
 
     @GetMapping
     ApiResponse<List<UserResponse>> getUsers(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("Username: {}", authentication.getName());
+        authentication.getAuthorities().forEach(grantedAuthority -> log.info(grantedAuthority.getAuthority()));
 
         return ApiResponse.<List<UserResponse>>builder()
-                .result(userservice.getUsers())
+                .result(userService.getUsers())
                 .build();
     }
 
     @GetMapping("/{userId}")
-    UserResponse getUser(@PathVariable String userId) {
-        return userservice.getUser(userId);
-    }
-
-    @PutMapping("/{userId}")
-    UserResponse updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
-        return userservice.updateUser(userId, request);
+    ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId){
+        return ApiResponse.<UserResponse>builder()
+                .result(userService.getUser(userId))
+                .build();
     }
 
     @DeleteMapping("/{userId}")
-    String deleteUser(@PathVariable String userId) {
-        userservice.deleteUser(userId);
-        return "Delete User Successfully!";
+    ApiResponse<String> deleteUser(@PathVariable String userId){
+        userService.deleteUser(userId);
+        return ApiResponse.<String>builder()
+                .result("User has been deleted")
+                .build();
+    }
+
+    @PutMapping("/{userId}")
+    ApiResponse<UserResponse> updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request){
+        return ApiResponse.<UserResponse>builder()
+                .result(userService.updateUser(userId, request))
+                .build();
     }
 }
