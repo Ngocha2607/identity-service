f541e4f29105981fc63a4f5af69da8a16765f8f4
dominic <dominic@tweetworldtravel.com>
2024-08-26 13:04:13 +0700

    update

---
+++ src/main/java/com/ngocha/identity_service/service/PermissionService.java
@@ -0,0 +1,17 @@
+package com.ngocha.identity_service.service;
+
+import com.ngocha.identity_service.repository.PermissionRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class PermissionService {
+    PermissionRepository permissionRepository;
+    String create(Object request) {
+        return "";
+    }
+}
